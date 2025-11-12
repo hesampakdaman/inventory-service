a9d61459c6e39d82065a0b85846cb2cf6d3bc365
Hesam Pakdaman <hesam.pakdaman@gmail.com>
2025-11-12 17:51:06 +0100

    feat: add bus to Service

--- internal/service/service.go
+++ internal/service/service.go
@@ -3,15 +3,17 @@ package service
 import (
 	"log/slog"
 
+	"github.com/hesampakdaman/inventory-service/internal/core/bus"
 	"github.com/hesampakdaman/inventory-service/internal/ports"
 )
 
 type Service struct {
 	repo   ports.Repository
 	logger *slog.Logger
+	bus    *bus.Bus
 }
 
-func New(repo ports.Repository, logger *slog.Logger) *Service {
+func New(repo ports.Repository, logger *slog.Logger, bus *bus.Bus) *Service {
 	logger = logger.With("component", "Service")
-	return &Service{repo: repo, logger: logger}
+	return &Service{repo: repo, logger: logger, bus: bus}
 }
